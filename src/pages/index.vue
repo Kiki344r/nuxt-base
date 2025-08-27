9cf3d5cf0284a9b1dfb8ce699454f87fb57b2631
Kilian <fol.kilian@spock-bot.xyz>
2025-08-27 15:27:11 +0200

    Initial Commit

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="h-screen w-full justify-center items-center flex">
-    <h1 class="text-2xl text-red-400 m-4 animate-ping"> Hello World</h1>
-  </div>
+
 </template>
 
 <script setup lang="ts">
